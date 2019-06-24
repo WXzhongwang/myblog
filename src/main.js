5eb62d86c4a725a228408a3efd2e10d95096a391
zhongwang <1528683621@qq.com>
2019-06-24 13:58:17 +0800

    main

--- src/main.js
+++ src/main.js
@@ -10,9 +10,19 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 
-import Mock from './mock/mock' 
+//import Mock from './mock/mock' 
 //axios.defaults.baseURL = 'http://mockjs.com' 
-//axios.defaults.baseURL = 'http://localhost:8080/ssm' 
+axios.defaults.baseURL = '/api';
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.transformRequest = [function (data) {
+    let ret = ''
+    for (let it in data) {
+      ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+    }
+    return ret
+}]
+
 Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
